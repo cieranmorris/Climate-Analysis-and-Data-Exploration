a68ccc58c82ff727d4e78b539eea7c114a39a116
Ciera Morris <cieranmorris@gmail.com>
2021-03-20 14:50:46 -0600

    troubleshooting file preview

--- sqlalchemy_app.py
+++ sqlalchemy_app.py
@@ -1,1 +1,12 @@
-sqlalchemy_pp.py
+
+#Import modules for analysis
+import sqlalchemy
+import datetime as dt
+from sqlalchemy.orm import Session
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy import create_engine, func, desc, inspect
+from flask import Flask, jsonify
+
+#create engine to sqlite file containing data
+engine = create_engine("sqlite:///Instructions/Resources/hawaii.sqlite")
+
